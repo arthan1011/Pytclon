90553aca407e9116fe37cab6ca65f115fa009dc6
arthan1011 <arthan1011@gmail.com>
2016-02-23 17:41:33 +0300

    gui: login test

--- src/test/java/org/arthan/pytclon/service/PlayerServiceTestGuiIT.java
+++ src/test/java/org/arthan/pytclon/service/PlayerServiceTestGuiIT.java
@@ -2,6 +2,7 @@ package org.arthan.pytclon.service;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -47,14 +48,40 @@ public class PlayerServiceTestGuiIT {
 
     @FindBy(id = "username")
     private WebElement loginInput;
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+    @FindBy(id = "loginSubmit")
+    private WebElement loginSubmit;
 
     @Test
-    public void should_test_gui() throws Exception {
+    public void should_access_main_page() throws Exception {
         browser.get(deploymentUrl.toExternalForm());
 
-        loginInput.sendKeys("python");
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals("Should be on login page", "Login Page", pageTitle);
 
-        String text = loginInput.getText();
-        Assert.assertTrue(loginInput.getText().contains("python"));
+        loginInput.sendKeys("python");
+        Assert.assertTrue(loginInput.getAttribute("value").contains("python"));
+        passwordInput.sendKeys("letmein");
+        Graphene.guardHttp(loginSubmit).click();
+
+        pageTitle = browser.getTitle();
+        Assert.assertEquals("Should be on main page", "Main Page", pageTitle);
+    }
+
+    @Test
+    public void should_get_error_page_on_failed_login() throws Exception {
+        browser.get(deploymentUrl.toExternalForm());
+
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals("Should be on login page", "Login Page", pageTitle);
+
+        loginInput.sendKeys("python");
+        Assert.assertTrue(loginInput.getAttribute("value").contains("python"));
+        passwordInput.sendKeys("wrong_password");
+        Graphene.guardHttp(loginSubmit).click();
+
+        pageTitle = browser.getTitle();
+        Assert.assertEquals("Should be on main page", "Error Page", pageTitle);
     }
 }
