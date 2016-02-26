83c0284ec20369c480594fc37d7b9f53ed6588bf
Artur Shamsiev <arthan1011@gmail.com>
2016-02-26 13:27:24 +0300

    custom dojo widget sample

---
+++ src/main/webapp/js/pytclon/ContentHolder.js
@@ -0,0 +1,25 @@
+/**
+ * Created by ashamsiev on 26.02.2016.
+ */
+
+define([
+    "dojo/_base/declare",
+    "dojo/dom-construct",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojo/text!./templates/ContentHolder.html"
+], function(
+    declare,
+    domConstruct,
+    _WidgetBase,
+    _TemplatedMixin,
+    template
+) {
+    return declare("ContentHolder", [_WidgetBase, _TemplatedMixin], {
+        templateString: template,
+
+        add: function(node) {
+            domConstruct.place(node, this.content);
+        }
+    });
+});
