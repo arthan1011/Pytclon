ca8f8cf63e8b6fc2c6f5d2a22fe89ca909b13e6f
Artur Shamsiev <arthan1011@gmail.com>
2016-03-01 17:52:16 +0300

    User password excluded from json serialization

--- src/main/java/org/arthan/pytclon/domain/entity/User.java
+++ src/main/java/org/arthan/pytclon/domain/entity/User.java
@@ -1,5 +1,8 @@
 package org.arthan.pytclon.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -18,12 +21,25 @@ public class User implements Serializable {
     @Column(name = "login")
     private String login;
 
+    @JsonIgnore
+    @Column(name = "passwd")
+    private String password;
+
     public String getLogin() {
         return login;
     }
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
