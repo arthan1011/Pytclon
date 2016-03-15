9889b33729b4d345ccef946fec499b6e651201b5
Artur Shamsiev <arthan1011@gmail.com>
2016-03-15 17:56:19 +0300

    admin page loading overlay added

--- src/main/webapp/js/app/adminApp.js
+++ src/main/webapp/js/app/adminApp.js
@@ -4,9 +4,32 @@
 
 require([
     'dojo/parser',
+    'dojo/_base/fx',
+    'dojo/ready',
+    'dojo/query',
+    'dojo/dom-style',
     'dojo/domReady!'
 ], function(
-    parser
+    parser,
+    fx,
+    ready,
+    query,
+    domStyle
 ) {
+    ready(function() {
+        var overlayNode = query('.loadingOverlay')[0];
+        fx.animateProperty({
+            node: overlayNode,
+            properties: {
+                top: '300',
+                opacity: '0'
+            },
+            onEnd: function(node) {
+                domStyle.set(node, {
+                    display: 'none'
+                });
+            }
+        }).play();
 
+    });
 });
