c725a2f748ceba61704ab532e9cee1d14ec1bdc8
Artur Shamsiev <arthan1011@gmail.com>
2016-04-26 10:18:52 +0300

    UserService test TODO

--- src/test/java/org/arthan/pytclon/service/UserServiceTest.java
+++ src/test/java/org/arthan/pytclon/service/UserServiceTest.java
@@ -9,4 +9,6 @@ import static org.junit.Assert.*;
  */
 public class UserServiceTest {
 
+    // TODO we should test that when new user is created his id is retrieved and used as user_id for a new default player
+    // that should be created right after new user creation
 }
