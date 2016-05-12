17c333e01bd2b2a6f58c2e8033f068f65247e236
arthan1011 <arthan1011@gmail.com>
2016-05-12 20:49:37 +0300

    User settings: dialog windows tabs begin

--- src/main/webapp/js/pytclon/dialog/userSettingsDialog.js
+++ src/main/webapp/js/pytclon/dialog/userSettingsDialog.js
@@ -7,29 +7,57 @@ define([
     'dijit/form/Button',
     'dijit/layout/BorderContainer',
     'dijit/layout/ContentPane',
+    'dojo/on',
+    'dojo/topic',
     'dojo/dom-construct'
 ], function (
     Dialog,
     Button,
     BorderContainer,
     ContentPane,
+    on,
+    topic,
     domConstruct
 ) {
+    const changeTabTopic = 'UserSettingsDialog_changeTab';
+
     var dialogContent = domConstruct.create('div');
 
     var bc = new BorderContainer({
         gutters: false,
         style: "width: 600px; height: 500px;"
     });
+
+    var tabs = domConstruct.create('div');
+    on(domConstruct.create('div', {
+        style: 'width: 100%; height: 35px; margin-bottom: 2px; background-color: red'
+    }, tabs), 'click', function () {
+        console.log('Red');
+        topic.publish(changeTabTopic, domConstruct.create('span', {innerHTML: 'RED'}));
+    });
+    on(domConstruct.create('div', {
+        style: 'width: 100%; height: 35px; margin-bottom: 2px; background-color: green'
+    }, tabs), 'click', function () {
+        console.log('Green');
+        topic.publish(changeTabTopic, domConstruct.create('span', {innerHTML: 'GREEN'}));
+    });
     var leftPane = new ContentPane({
         region: "left",
         style: "width: 150px; border-right: solid grey 1px",
-        content: 'Tabs'
+        content: tabs
     });
+
     var mainPane = new ContentPane({
         region: "center",
         content: 'Main'
     });
+
+    topic.subscribe(changeTabTopic, function (mainContent) {
+        domConstruct.empty(mainPane.domNode);
+        domConstruct.place(mainContent, mainPane.domNode);
+    });
+
+
     var bottomPanel = new ContentPane({
         region: "bottom",
         style: "border-top: solid grey 1px"
